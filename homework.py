030f23b93cdefde7ff4feb272437b89ad7c47bff
stevengao-ytc <stevengao213@gmail.com>
2026-06-14 12:18:20 +1000

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,7 +1,18 @@
-# Edit this file and commit changes to submit your homework.
+# Make a program that
+# -Keeps asking the user to enter a number until they until a zero
+# -Keeps track of the total sum.
 
-print("hello")
+# TO DO
 
+# example template (This is just one example of how you could do it. There are multiple ways to do this question
+# and you can do it any way you like.)
+
+total = 0
+number = int(input("..."))
+while number != 0:
+  # do something
+
+print("Total = " + str(total))
 
 
 
